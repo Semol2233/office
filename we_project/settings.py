516651ac8b7d9dbe2813a434babdc2bfb3a1084a
Semol <semolmahamud2233@gmail.com>
2020-11-08 21:30:00 +0600

    hdec

--- we_project/settings.py
+++ we_project/settings.py
@@ -125,10 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,),'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+#dcd
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL ='/media/'
+
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'login'
