53a6b88d2b91c26c703d4164072f80a9557bc7a4
Semol <semolmahamud2233@gmail.com>
2020-11-08 22:35:12 +0600

    hdesdddsc

--- login_register/admin.py
+++ login_register/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib.auth.models import  Group
-
+admin.site.site_header = "Vlan Management"
 
 admin.site.unregister(Group)
