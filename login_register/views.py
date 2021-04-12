d6c13116d71e1536b1262d3780051a9fe836c339
Semol <semolmahamud2233@gmail.com>
2021-04-12 16:50:26 +0600

    jws

--- login_register/views.py
+++ login_register/views.py
@@ -304,7 +304,7 @@ class daulycost_list(LoginRequiredMixin,ListView):
     model = dailybilling
     template_name= 'goninda/list.html'
     #queryset = dailybilling.objects.filter(created_date__gte=datetime.now() - timedelta(days=1))
-    queryset = dailybilling.objects.filter(date__gte=datetime.now()-timedelta(days=31)).exclude(cost_profile__cost_name__contains='Advance salary')
+    queryset = dailybilling.objects.filter(date__gte=datetime.now()-timedelta(days=31))
 
 
 
