32a9f87cf463e33084bbd6fd29e6528c63309117
Semol <semolmahamud2233@gmail.com>
2021-04-14 15:25:27 +0600

    hjnd

--- we_project/settings.py
+++ we_project/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'we_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
-        'USER': 'myprojectuser',
-        'PASSWORD': 'password',
+        'NAME': 'semol',
+        'USER': 'semol',
+        'PASSWORD': 'semol',
         'HOST': '192.168.0.189',
         'PORT': '5432',
     }
