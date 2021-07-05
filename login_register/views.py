3f37033d0b9874249e413072a4ec56d89b575c9b
Semol <semolmahamud2233@gmail.com>
2021-07-05 12:33:54 +0600

    djfruujujt

--- login_register/views.py
+++ login_register/views.py
@@ -461,7 +461,7 @@ class montlybillview(LoginRequiredMixin,ListView):
 
     def get_context_data(self, **kwargs):
          context = super(montlybillview, self).get_context_data(**kwargs)
-         context['alldata'] = monthlybill.objects.filter(headline__startswith="July")
+         context['alldata'] = monthlybill.objects.filter(month__month__startswith="July")
          context['totaluser'] = monthlybill.objects.all().exclude(month__month__contains="May").count()
          context['paiduser'] = monthlybill.objects.filter(payment_status=True).exclude(month__month__contains="May").count()
          context['unpaiduser'] = monthlybill.objects.filter(payment_status=False).exclude(month__month__contains="May").count()
