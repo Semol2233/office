0f37efd79a655585e75708cecaf5b33f107c11e7
Semol <semolmahamud2233@gmail.com>
2021-08-05 13:43:42 +0600

    sfheh

--- login_register/views.py
+++ login_register/views.py
@@ -329,7 +329,7 @@ class OFFICE_COST(LoginRequiredMixin,ListView):
     context_object_name = 'fulllist'
     model = dailybilling
     template_name= 'goninda/cost.html'
-    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Office Cost',date__range=["2021-06-11", "2021-07-11"])
+    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Office Cost',date__range=["2021-07-11", "2021-08-11"])
 
 
 
@@ -337,7 +337,7 @@ class OFFICE_bike(LoginRequiredMixin,ListView):
     context_object_name = 'fulllist'
     model = dailybilling
     template_name= 'goninda/cost.html'
-    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Bike Cost',date__range=["2021-06-11", "2021-07-11"])
+    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Bike Cost',date__range=["2021-07-11", "2021-08-11"])
 
 
 
@@ -345,14 +345,14 @@ class OFFICE_family(LoginRequiredMixin,ListView):
     context_object_name = 'fulllist'
     model = dailybilling
     template_name= 'goninda/cost.html'
-    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Family',date__range=["2021-06-11", "2021-07-11"])
+    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Family',date__range=["2021-07-11", "2021-08-11"])
 
 
 class OFFICE_product(LoginRequiredMixin,ListView):
     context_object_name = 'fulllist'
     model = dailybilling
     template_name= 'goninda/cost.html'
-    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Product Cost',date__range=["2021-06-11", "2021-07-11"])
+    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Product Cost',date__range=["2021-07-11", "2021-08-11"])
 
 
 
@@ -360,14 +360,14 @@ class OFFICE_salllery(LoginRequiredMixin,ListView):
     context_object_name = 'fulllist'
     model = dailybilling
     template_name= 'goninda/cost.html'
-    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Salary',date__range=["2021-06-11", "2021-07-11"])
+    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Salary',date__range=["2021-07-11", "2021-08-11")
 
 
 class OFFICE_trnasportcost(LoginRequiredMixin,ListView):
     context_object_name = 'fulllist'
     model = dailybilling
     template_name= 'goninda/cost.html'
-    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Transport Cost',date__range=["2021-06-11", "2021-07-11"])
+    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Transport Cost',date__range=["2021-07-11", "2021-08-11"])
 
 
 
@@ -375,21 +375,21 @@ class OFFICE_Chika(LoginRequiredMixin,ListView):
     context_object_name = 'fulllist'
     model = dailybilling
     template_name= 'goninda/cost.html'
-    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Chika',date__range=["2021-06-11", "2021-07-11"])
+    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Chika',date__range=["2021-07-11", "2021-08-11"])
 
 
 class OFFICE_internetbill(LoginRequiredMixin,ListView):
     context_object_name = 'fulllist'
     model = dailybilling
     template_name= 'goninda/cost.html'
-    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='orange',date__range=["2021-06-11", "2021-07-11"])
+    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='orange',date__range=["2021-07-11", "2021-08-11"])
 
 
 class OFFICE_Electric(LoginRequiredMixin,ListView):
     context_object_name = 'fulllist'
     model = dailybilling
     template_name= 'goninda/cost.html'
-    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Electric Cost',date__range=["2021-06-11", "2021-07-11"])
+    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Electric Cost',date__range=["2021-07-11", "2021-08-11"])
 
 
 
@@ -397,7 +397,7 @@ class OFFICE_Employ(LoginRequiredMixin,ListView):
     context_object_name = 'fulllist'
     model = dailybilling
     template_name= 'goninda/cost.html'
-    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Employ Cost',date__range=["2021-06-11", "2021-07-11"])
+    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Employ Cost',date__range=["2021-07-11", "2021-08-11"])
 
 
 
@@ -405,7 +405,7 @@ class OFFICE_Pickup(LoginRequiredMixin,ListView):
     context_object_name = 'fulllist'
     model = dailybilling
     template_name= 'goninda/cost.html'
-    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Pickup Cost',date__range=["2021-06-11", "2021-07-11"])
+    queryset = dailybilling.objects.filter(cost_profile__cost_name__contains='Pickup Cost',date__range=["2021-07-11", "2021-08-11"])
 
 
 
