19be7936ea76b8ddf74a8f4773409d7dd67f20e6
Semol <semolmahamud2233@gmail.com>
2021-09-13 17:44:44 +0600

    hch

--- login_register/views.py
+++ login_register/views.py
@@ -424,13 +424,13 @@ class updatedailyline(LoginRequiredMixin,ListView):
 
 
 def updatessdata(request):
-    allupdatedate = userupdate.objects.filter(date_user__range=[month])
-    lastconnection = userupdate.objects.filter(date_user__range=[month]).count()
-    totalsilveruser = userupdate.objects.filter(date_user__range=[month],pkg_namess__pkgname__startswith="Silver").count()
-    totalgoldenuser = userupdate.objects.filter(date_user__range=[month],pkg_namess__pkgname__startswith="Gold").count()
-    totalskyenuser = userupdate.objects.filter(date_user__range=[month],pkg_namess__pkgname__startswith="Sky").count()
-    totaldaimondenuser = userupdate.objects.filter(date_user__range=[month],pkg_namess__pkgname__startswith="Daimond").count()
-    totalstarenuser = userupdate.objects.filter(date_user__range=[month],pkg_namess__pkgname__startswith="Star").count()
+    allupdatedate = userupdate.objects.filter(date_user__range=month)
+    lastconnection = userupdate.objects.filter(date_user__range=month).count()
+    totalsilveruser = userupdate.objects.filter(date_user__range=month,pkg_namess__pkgname__startswith="Silver").count()
+    totalgoldenuser = userupdate.objects.filter(date_user__range=month,pkg_namess__pkgname__startswith="Gold").count()
+    totalskyenuser = userupdate.objects.filter(date_user__range=month,pkg_namess__pkgname__startswith="Sky").count()
+    totaldaimondenuser = userupdate.objects.filter(date_user__range=month,pkg_namess__pkgname__startswith="Daimond").count()
+    totalstarenuser = userupdate.objects.filter(date_user__range=month,pkg_namess__pkgname__startswith="Star").count()
     return render(request,"goninda/dalyconnection.html",{"dataone":allupdatedate,"datatwo":lastconnection,"usertype1":totalsilveruser,"usertype2":totalgoldenuser,"usertype3":totalskyenuser,"usertype4":totaldaimondenuser,"usertype5":totalstarenuser})
 
 
