8c54d7a6acbcd9b0685ab33f9987e32ec459ffe3
Semol <semolmahamud2233@gmail.com>
2021-09-20 10:56:19 +0600

    fhjsfs

--- login_register/date.py
+++ login_register/date.py
@@ -3,5 +3,5 @@ month = "September"
 months = ["2021-09-12", "2021-09-25"]
 routersdfcd = ["2021-08-10", "2021-09-10"]
 othercharge = ["2021-01-1", "2021-09-10"]
-newconnection_date = ["2021-01-1", "2021-09-10"]
+newconnection_date = ["2021-09-11", "2021-10-11"]
 
