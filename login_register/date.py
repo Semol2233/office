6b2d7bba860d0399ecadd10b2d46d0bd2be41a4d
Semol <semolmahamud2233@gmail.com>
2021-10-18 18:20:28 +0600

    fg

--- login_register/date.py
+++ login_register/date.py
@@ -2,10 +2,10 @@ datedata = ["2021-09-11", "2021-10-11"]
 month = "October"
 months = ["2021-09-14", "2021-10-15"]
 
-routersdfcd = ["2021-08-10", "2021-09-10"]
+routersdfcd = ["2021-08-10", "2022-12-30"]
 
 othercharge = ["2021-01-1", "2021-09-11"]
-newconnection_date = ["2021-10-11", "2021-11-11"]
+newconnection_date = ["2021-10-11", "2021-10-13"]
 loondate = ["2021-10-11", "2021-11-11"]
 
 #pevmonth_date
