20c7f5b6a437e522d551dafb5306760912723515
Semol <semolmahamud2233@gmail.com>
2021-10-18 13:26:19 +0600

    hds

--- login_register/date.py
+++ login_register/date.py
@@ -2,8 +2,9 @@ datedata = ["2021-09-11", "2021-10-11"]
 month = "October"
 months = ["2021-09-14", "2021-10-15"]
 routersdfcd = ["2021-08-10", "2021-09-10"]
-othercharge = ["2021-01-1", "2021-09-10"]
-newconnection_date = ["2021-09-11", "2021-10-11"]
+
+othercharge = ["2021-01-1", "2021-09-11"]
+newconnection_date = ["2021-10-11", "2021-11-11"]
 
 #pevmonth_date
 monthpev = "September"
