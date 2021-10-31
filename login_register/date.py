bd174ec34b988f8844931be9596df60991c91a71
Semol <semolmahamud2233@gmail.com>
2021-10-31 17:33:41 +0600

    gfg

--- login_register/date.py
+++ login_register/date.py
@@ -1,5 +1,6 @@
 datedata = ["2021-10-11", "2021-11-10"]
-month = "October"
+month = "November"
+
 months = ["2021-10-11", "2021-11-10"]
 
 routersdfcd = ["2021-08-10", "2021-10-13"]
@@ -9,8 +10,8 @@ newconnection_date = ["2021-10-11", "2021-11-10"]
 loondate = ["2021-10-11", "2021-11-11"]
 
 #pevmonth_date
-monthpev = "September"
-decline_pevmonth = "September"
-unpaid_pevmonth = "September"
+monthpev = "October"
+decline_pevmonth = "October"
+unpaid_pevmonth = "October"
 monthss = ["2021-10-14", "2021-11-25"]
-month_col = "November"
+month_col = "October"
