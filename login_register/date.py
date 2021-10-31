1af92a32820580daf9514da64b95391285af03b7
Semol <semolmahamud2233@gmail.com>
2021-10-31 12:33:38 +0600

    gfg

--- login_register/date.py
+++ login_register/date.py
@@ -5,7 +5,7 @@ months = ["2021-10-11", "2021-11-10"]
 routersdfcd = ["2021-08-10", "2021-10-13"]
 
 othercharge = ["2021-01-1", "2021-09-11"]
-newconnection_date = ["2021-01-11", "2021-11-10"]
+newconnection_date = ["2021-10-11", "2021-11-10"]
 loondate = ["2021-10-11", "2021-11-11"]
 
 #pevmonth_date
